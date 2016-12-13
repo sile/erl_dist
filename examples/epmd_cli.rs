16c53a0bce1ec7135b4d7aecf18c708ecc5f650f
Takeru Ohta <phjgt308@gmail.com>
2016-12-14 04:30:19 +0900

    Implement 'names' request

--- examples/epmd_cli.rs
+++ examples/epmd_cli.rs
@@ -5,71 +5,29 @@ extern crate futures;
 extern crate handy_async;
 extern crate clap;
 
-use std::io::{Write, Read, Sink};
-use futures::Future;
+use clap::{App, Arg, SubCommand};
 use fibers::Executor;
-use fibers::net::TcpStream;
-use handy_async::pattern::{Pattern, Endian, BoxPattern};
-use handy_async::pattern::combinators::BE;
-use handy_async::pattern::read::{U8, U16, U32};
-use handy_async::io::misc::Counter;
-use handy_async::io::{ReadFrom, PatternReader};
-use handy_async::io::{WriteInto, PatternWriter};
 
 fn main() {
+    let matches = App::new("epmd_cli")
+        .arg(Arg::with_name("EPMD_HOST").short("h").takes_value(true).default_value("127.0.0.1"))
+        .arg(Arg::with_name("EPMD_PORT").short("p").takes_value(true).default_value("4369"))
+        .subcommand(SubCommand::with_name("names"))
+        .get_matches();
+    let epmd_host = matches.value_of("EPMD_HOST").unwrap();
+    let epmd_port = matches.value_of("EPMD_PORT").unwrap();
+    let epmd_addr = format!("{}:{}", epmd_host, epmd_port).parse().expect("Invalid epmd address");
+
+    let client = erl_dist::epmd::Client::new(&epmd_addr);
     let mut executor = fibers::InPlaceExecutor::new().unwrap();
-    let monitor = executor.spawn_monitor(TcpStream::connect("127.0.0.1:4369".parse().unwrap())
-        .and_then(|socket| {
-            let local = socket.local_addr().unwrap();
-            println!("Connected: local={}", local);
-            let alive2_req = alive2_req(local.port());
-            alive2_req.write_into(socket).map_err(|e| e.into_error())
-        })
-        .and_then(|(socket, _)| {
-            println!("Sent: alive2_req");
-            alive2_resp().read_from(socket).map_err(|e| e.into_error())
-        })
-        .and_then(|(socket, resp)| {
-            println!("Recv: alive2_resp: {:?}", resp);
-            port_req("foo").write_into(socket).map_err(|e| e.into_error())
-        })
-        .and_then(|(socket, _)| {
-            println!("Sent: port_req");
-            port_resp().read_from(socket).map_err(|e| e.into_error())
-        })
-        .and_then(|(socket, resp)| {
-            println!("Recv: port_resp: {:?}", resp);
-            Ok(())
-        }));
-    let r = executor.run_fiber(monitor).unwrap();
-    println!("{:?}", r);
-}
 
-fn port_req<W: Write + Send + 'static>(name: &str) -> BoxPattern<PatternWriter<W>, ()> {
-    reqfmt((122u8, name.to_string())).map(|_| ()).boxed()
-}
-
-fn port_resp<R: Read + Send + 'static>() -> BoxPattern<PatternReader<R>, u8> {
-    U8.boxed()
-}
-
-fn reqfmt<P: WriteInto<Counter<Sink>> + Clone>(req: P) -> (BE<u16>, P) {
-    let counter = Counter::with_sink();
-    let (counter, _) = req.clone().write_into(counter).wait().unwrap();
-    ((counter.written_size() as u16).be(), req)
-}
-
-fn alive2_req<W: Write + Send + 'static>(port: u16) -> BoxPattern<PatternWriter<W>, ()> {
-    reqfmt((120u8, port.be(), 77u8, 0, 5, 5, ("bar".len() as u16).be(), "bar".to_string(), 0))
-        .map(|_| ())
-        .boxed()
-}
-
-fn alive2_resp<R: Read + Send + 'static>() -> BoxPattern<PatternReader<R>, (u8, u16)> {
-    (U8, U8, U16.be())
-        .map(|(tag, result, creation)| {
-            assert_eq!(tag, 121);
-            (result, creation)
-        })
-        .boxed()
+    if let Some(_matches) = matches.subcommand_matches("names") {
+        let monitor = executor.spawn_monitor(client.names());
+        let names = executor.run_fiber(monitor).unwrap().expect("'names' request failed");
+        println!("Registered Names");
+        println!("================\n");
+        println!("{}", names);
+    } else {
+        println!("{}", matches.usage());
+    }
 }
--- src/epmd.rs
+++ src/epmd.rs
@@ -1,1 +1,72 @@
-pub struct Client;
+use std::io::{Sink, Error};
+use std::net::SocketAddr;
+use futures::{Future, BoxFuture};
+use fibers;
+use handy_async::pattern::{Pattern, Endian};
+use handy_async::pattern::combinators::BE;
+use handy_async::pattern::read::{until, U32};
+use handy_async::io::misc::Counter;
+use handy_async::io::{ReadFrom, WriteInto};
+
+pub const TAG_NAMES_REQ: u8 = 110;
+
+pub struct Client {
+    server_addr: SocketAddr,
+}
+impl Client {
+    pub fn new(server_addr: &SocketAddr) -> Self {
+        Client { server_addr: server_addr.clone() }
+    }
+    pub fn names(&self) -> BoxFuture<String, Error> {
+        self.connect()
+            .and_then(|socket| {
+                let req = request(TAG_NAMES_REQ);
+                req.write_into(socket).map_err(|e| e.into_error())
+            })
+            .and_then(|(socket, _)| {
+                // TODO:
+                let resp = (U32.be(), until(|_, eos| if eos { Ok(Some(())) } else { Ok(None) }))
+                    .map(|(_, (s, _))| String::from_utf8(s).unwrap());
+                resp.read_from(socket).map(|(_, s)| s).map_err(|e| e.into_error())
+            })
+            .boxed()
+    }
+
+    fn connect(&self) -> fibers::net::futures::Connect {
+        fibers::net::TcpStream::connect(self.server_addr.clone())
+    }
+}
+
+fn request<P: WriteInto<Counter<Sink>> + Clone>(req: P) -> (BE<u16>, P) {
+    let counter = Counter::with_sink();
+    let (counter, _) = req.clone().write_into(counter).wait().unwrap();
+    ((counter.written_size() as u16).be(), req)
+}
+
+// fn port_req<W: Write + Send + 'static>(name: &str) -> BoxPattern<PatternWriter<W>, ()> {
+//     reqfmt((122u8, name.to_string())).map(|_| ()).boxed()
+// }
+
+// fn port_resp<R: Read + Send + 'static>() -> BoxPattern<PatternReader<R>, u8> {
+//     U8.boxed()
+// }
+
+
+// fn strbuf(len: usize) -> String {
+//     String::from_utf8(vec![0; len]).unwrap()
+// }
+
+// fn alive2_req<W: Write + Send + 'static>(port: u16) -> BoxPattern<PatternWriter<W>, ()> {
+//     reqfmt((120u8, port.be(), 77u8, 0, 5, 5, ("bar".len() as u16).be(), "bar".to_string(), 0))
+//         .map(|_| ())
+//         .boxed()
+// }
+
+// fn alive2_resp<R: Read + Send + 'static>() -> BoxPattern<PatternReader<R>, (u8, u16)> {
+//     (U8, U8, U16.be())
+//         .map(|(tag, result, creation)| {
+//             assert_eq!(tag, 121);
+//             (result, creation)
+//         })
+//         .boxed()
+// }
